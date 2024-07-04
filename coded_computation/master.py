554a87587f799e1c59044f5987e4bc09e160c853
willem-strydom <s.willem@wustl.edu>
2024-07-03 20:59:51 -0800

    removed robustness checks

--- coded_computation/master.py
+++ coded_computation/master.py
@@ -137,11 +137,10 @@ class master:
         # get the correct parity from master
         if w.shape[0] == 1:
             parity = np.hstack([node.row_parity for node in self.nodes_list]).reshape(1, -1)
-            assert np.allclose(parity, self.row_parity, atol=1e-5)
+            # assert np.allclose(parity, self.row_parity, atol=1e-5)
         elif w.shape[1] == 1:
             parity = np.sum([node.col_parity for node in self.nodes_list], axis=0)
-            # print(np.hstack((parity, Master.col_parity)))
-            assert np.allclose(parity, self.col_parity, atol=1e-5)
+            # assert np.allclose(parity, self.col_parity, atol=1e-5)
 
         # add up responses according to algorithm
 
@@ -154,13 +153,13 @@ class master:
 
         # ensure that query is done correctly
 
-        if not np.allclose(response.reshape(-1, 1), actual.reshape(-1, 1), atol=1e-3):
-            error = np.linalg.norm(response - actual)
-            print("response, actual \n", np.hstack((response.reshape(-1, 1)[0:5], actual.reshape(-1, 1)[0:5])), "\n")
-            print(f"index passed: {index}")
-            print(f"tabel: {query_table}")
-            print(f"how we used to make the index: {impute(values, expected_len, dict).reshape(-1, 1)}")
-            raise ValueError(f"query does not work: from w = {np.unique(w_flat).reshape(-1, 1)}, with error: {error}, shape :{w.shape}")
+        # if not np.allclose(response.reshape(-1, 1), actual.reshape(-1, 1), atol=1e-3):
+        #     error = np.linalg.norm(response - actual)
+        #     print("response, actual \n", np.hstack((response.reshape(-1, 1)[0:5], actual.reshape(-1, 1)[0:5])), "\n")
+        #     print(f"index passed: {index}")
+        #     print(f"tabel: {query_table}")
+        #     print(f"how we used to make the index: {impute(values, expected_len, dict).reshape(-1, 1)}")
+        #     raise ValueError(f"query does not work: from w = {np.unique(w_flat).reshape(-1, 1)}, with error: {error}, shape :{w.shape}")
 
         return response
 
