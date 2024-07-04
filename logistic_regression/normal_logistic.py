cf591ecc3ebe95f8d79a1e576049e2f3efc309c8
willem-strydom <s.willem@wustl.edu>
2024-07-03 16:47:25 -0800

    added stable logistic to unquantized sys

--- logistic_regression/normal_logistic.py
+++ logistic_regression/normal_logistic.py
@@ -1,25 +1,21 @@
-from stable_logistic import stable_sigmoid, stable_loss
 import numpy as np
+from logistic_regression.stable_logistic import stable_sigmoid, stable_loss
+
 
 def normallogistic(w, Master,yTr, X):
-    """y_pred = w.T @ xTr
-    loss = np.sum(np.log(1 + np.exp(-yTr * y_pred)))
-    num = yTr * xTr
-    den = (1 + np.exp(yTr * (y_pred)))
-    gradient = -np.sum((num / den), axis=1).reshape(-1, 1)"""
-    dictionary = {}
+    """
+    :param w: model
+    :param Master: has coded data
+    :param yTr: labels in +-1
+    :param X: raw data
+    :return: loss, gradient
+    """
+    dictionary = {}  # positional arg for master.query
     y_pred = Master.query(w, X, dictionary)
-    loss = np.sum(np.log(1 + np.exp(-yTr * y_pred)))
-    den = (1 + np.exp(yTr * y_pred))
-    alpha = yTr / den
+    vals = yTr * y_pred
+    loss = stable_loss(-vals)
+    func = np.vectorize(stable_sigmoid)
+    den = func(-vals)  # computes sigmoid func 1/1+e^{-x}, so pass -vals for correct gradient
+    alpha = yTr * den
     gradient = - Master.query(alpha.reshape(1, -1), X, dictionary).reshape(-1, 1)/len(yTr)
-
-    """y_pred = X @ w
-    num = X * yTr
-    den = (1 + np.exp(yTr * (y_pred)))
-    actual_gradient = -np.sum((num / den), axis=0).reshape(-1, 1)
-
-    print(actual_gradient.shape, gradient.shape)
-    if not np.allclose(actual_gradient, gradient):
-        raise ValueError(f"actual, {actual_gradient[0:5]}, ours {gradient[0:5]}")"""
     return loss, gradient
