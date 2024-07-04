650e9a44ea73130fcee9a23191d9e1809d555279
willem-strydom <s.willem@wustl.edu>
2024-07-03 16:11:59 -0800

    change quant logistic to stable

--- logistic_regression/quant_logistic.py
+++ logistic_regression/quant_logistic.py
@@ -1,7 +1,7 @@
 import numpy as np
-import pandas as pd
 from quantization.quantize import quantize
-
+from logistic_regression.stable_logistic import stable_sigmoid, stable_loss
+from util import record_access
 
 def quant_logistic(w, Master, w_lvl, grd_lvl, dict, X, y, filename, index):
     """
@@ -14,20 +14,18 @@ def quant_logistic(w, Master, w_lvl, grd_lvl, dict, X, y, filename, index):
     :param y: labels
     :param filename: where dict is recorded as csv
     :param index: for lookup table to make +-1 queries
-    :return: gradient, as numpy column vector
+    :return: loss, gradient as numpy column vector
     """
-    #y_pred = w.T @ xTr ... now with low access
     y_pred = Master.uniform_query(w, w_lvl, dict, X, index)
-    vals = y * y_pred
-    loss = np.mean(np.log(1 + np.exp(-vals)))
     record_access(dict, filename)
 
-    func = lambda x: 1 / (1 + np.exp(x))
-    func = np.vectorize(func)
-    vals = func(vals)
-
-    # then quantize y_i*alpha_i
-    # reset dict
+    vals = y * y_pred
+    loss = stable_loss(-vals)  # computation of loss robust to overflow
+    func = np.vectorize(stable_sigmoid)
+    den = func(-vals)  # computes 1/1+e^{-x}, so should passs -vals for correct computation
+    alpha = den*y
+    alpha, index = quantize(alpha, grd_lvl, "unif")
+    alpha = alpha.reshape(1,-1)  # alpha is a row vector
     dict = {
         'w-quantization': [w_lvl],
         'grd-quantization': [grd_lvl],
@@ -37,21 +35,10 @@ def quant_logistic(w, Master, w_lvl, grd_lvl, dict, X, y, filename, index):
         'time': [0],
         'stop cond': [0],
         'iters': [0],
-        'e in': [0],
-        'e out': [0]
+        'e in': [-1],
+        'e out': [-1]
     }
-    vals = vals*y
-    alpha, index = quantize(vals, grd_lvl, "unif")
-    alpha = alpha.reshape(1,-1)
     gradient = - Master.uniform_query(alpha, grd_lvl, dict, X, index)/len(y)
     record_access(dict, filename)
-    gradient = gradient.reshape(-1,1)
+    gradient = gradient.reshape(-1,1)  # gradient, model are column vectors always
     return loss, gradient
-
-
-
-def record_access(dict, filename):
-
-    df = pd.DataFrame(dict)
-    df.to_csv(filename, mode='a', index=False, header=False)
-
